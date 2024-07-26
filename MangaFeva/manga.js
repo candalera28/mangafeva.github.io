c64e9872833f14f389d2576013f987eec16a90df
candalera28 <candalerajohnneil@gmail.com>
2024-07-26 23:32:39 +0800

    Add files via upload

---
+++ MangaFeva/manga.js
@@ -0,0 +1,13 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navigationbuttons = document.querySelector('.navigation-buttons');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navigationbuttons.classList.toggle('active');
+}
+
+
+
+
+
+//For 2nd pages of manga (ALL)//
